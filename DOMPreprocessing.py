3c75c51e91df24e2db20f0f1a55e8c81a75ebf2e
Muneeb Muhammad <muneebkattak57@gmail.com>
2023-06-16 16:36:55 +0500

    error in beautiful soup correct. and wait for user to enter before getting page source

--- DOMPreprocessing.py
+++ DOMPreprocessing.py
@@ -34,8 +34,9 @@ chrome_options.add_argument(f'user-agent={os.getenv("userAgent")}')
 driver = webdriver.Chrome(service=service)
 
 
-url = "https://stackoverflow.com/users/signup?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f"
+url = "https://qalam.nust.edu.pk/"
 driver.get(url)
+input("Enter")
 source = driver.page_source
 soup = BeautifulSoup(source, "html.parser")
 
@@ -71,31 +72,33 @@ f.write("Title : " + title.text)
 for i in orderdElement:
     # write the element name and, its type and placeholder if input field or text if not input filed
     d = i.name + " : " + (i.get("type").strip() + " : " + (i.get("name") if "name" in i.attrs else "No Name") if i.name == "input" else i.get_text().strip().replace("\n", " "))
-    f.write('\n'+d)
+    try:
+        f.write('\n'+d)
+    except:
+        print("unicode not available")
 
 # this will convert the beautiful soup tag in orderedElement[index] to interactable selenium object
-def getSeleniumElement(index):
-    attributes = orderdElement[index].attrs
-    seleniumElement = None
-    try:
-        if 'id' in attributes:
-            seleniumElement = driver.find_element(By.ID, attributes["id"])
-        elif 'class' in attributes:
-            classes = ' '.join(attributes['class'])
-            seleniumElement = driver.find_element(By.XPATH, f"//*[@class='{classes}']")
-    except:
-        print("Error: Can't get the selenium element")
-    return seleniumElement
+# def getSeleniumElement(index):
+#     attributes = orderdElement[index].attrs
+#     seleniumElement = None
+#     try:
+#         if 'id' in attributes:
+#             seleniumElement = driver.find_element(By.ID, attributes["id"])
+#         elif 'class' in attributes:
+#             classes = ' '.join(attributes['class'])
+#             seleniumElement = driver.find_element(By.XPATH, f"//*[@class='{classes}']")
+#     except:
+#         print("Error: Can't get the selenium element")
+#     return seleniumElement
 
 # after element selected perform relevant action for each element
-def performElementAction(seleniumElement):
-    print(seleniumElement)
+def actionHelperFunction(seleniumElement, bsElement):
     tag_name = seleniumElement.tag_name
     try:
         if tag_name == 'a' or tag_name == 'button':
             seleniumElement.click()
         elif tag_name == 'input' or tag_name == 'password':
-            if seleniumElement.type == 'submit' or seleniumElement.type == 'button':
+            if (bsElement.attrs)["type"] == 'submit' or (bsElement.attrs)["type"] == 'button':
                 seleniumElement.click()
             else:
                 print("waiting for user to fill fields")
@@ -104,7 +107,44 @@ def performElementAction(seleniumElement):
     except:
         print("Interaction Error. User take step")
 
-# input ("enter")
+# after element selected perform relevant action for each element
+def performAction(index):
+    bsElement = orderdElement[index]
+    tag_name = bsElement.name
+    attributes = bsElement.attrs
+
+    if "id" in bsElement.attrs:
+        seleniumElement = driver.find_element(By.ID, attributes["id"])
+        actionHelperFunction(seleniumElement, bsElement)
+    else:
+        try:
+            if tag_name == 'a':
+                seleniumElement = driver.find_element(By.XPATH, f"//a[@href='{attributes['href']}']")
+                seleniumElement.click()
+            elif tag_name == 'button':
+                seleniumElement = driver.find_element(By.XPATH, f"//button[text()='{bsElement.text}']")
+                seleniumElement.click()
+            elif tag_name == 'input' or tag_name == 'password':
+                css_selector = tag_name
+                for attr, value in (bsElement.attrs).items():
+                    if attr == "class": continue
+                    css_selector += '[{}="{}"]'.format(attr, value)
+                seleniumElement = driver.find_element(By.CSS_SELECTOR, css_selector)
+
+                if attributes["type"] == 'submit' or attributes["type"] == 'button':
+                    seleniumElement.click()
+                else:
+                    print("waiting for user to fill fields")
+            else:
+                print("text:", bsElement.text)
+
+        except:
+            print("Error: can't find element")
+
+
+
+d = performAction(1)
+
 # de = getSeleniumElement(0)
 #
 # performElementAction(de)
