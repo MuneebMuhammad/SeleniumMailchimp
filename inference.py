69afae4dea2088808836f8a8703e521ca89a9094
Muneeb Muhammad <muneebkattak57@gmail.com>
2023-07-13 12:30:58 +0500

    Training and inference code set. Also made dataset creation pipeline

--- inference.py
+++ inference.py
@@ -6,16 +6,17 @@ from datasets import load_dataset
 
 dataset = load_dataset('json', data_files='data.json')
 
+# load pretrained model
 feature_extractor = MarkupLMFeatureExtractor()
 tokenizer = MarkupLMTokenizerFast.from_pretrained("my_qa_model")
 processor = MarkupLMProcessor(feature_extractor, tokenizer)
-
 data_collator = DefaultDataCollator()
 model = MarkupLMForQuestionAnswering.from_pretrained("my_qa_model/checkpoint-12")
 
 #inference
 with torch.no_grad():
-    inputs = processor(dataset["train"][3]["context"], questions=dataset["train"][0]["question"], return_tensors="pt", truncation=True, max_length=512, padding=True)
+    inputs = processor(dataset["train"][1]["context"], questions=dataset["train"][3]["question"], return_tensors="pt", truncation=True, max_length=512, padding='max_length', return_offsets_mapping=True)
+    offsets = inputs.pop("offset_mapping")
     outputs = model(**inputs)
 answer_start_index = outputs.start_logits.argmax()
 answer_end_index = outputs.end_logits.argmax()
--- training_QA.py
+++ training_QA.py
@@ -9,6 +9,7 @@ feature_extractor = MarkupLMFeatureExtractor()
 tokenizer = MarkupLMTokenizerFast.from_pretrained("microsoft/markuplm-base-finetuned-websrc")
 processor = MarkupLMProcessor(feature_extractor, tokenizer)
 
+# change markupLM offset into the offset of reqular tokenizer
 def offsetCumulation(offset):
     et = 0
     for r, i in enumerate(offset):
@@ -20,13 +21,19 @@ def offsetCumulation(offset):
         offset[r][1] = et+offset[r][1]
     return offset
 
+# preprocess the dataset
 def preprocessFunction(examples):
     questions = examples["question"].strip()
     inputs = processor(examples["context"], questions=questions, return_tensors="pt", truncation=True, return_offsets_mapping=True, return_special_tokens_mask=True, max_length=512, padding='max_length'
                        )
-    offset = inputs.pop("offset_mapping")
+    torch.set_printoptions(profile="full")  ###### remove this
+    # offset = inputs.pop("offset_mapping")  ############# uncomment this
+    offset = inputs["offset_mapping"]         ############## comment this
     offset = offset[0]
     offset = offsetCumulation(offset)
+    print("offset:", offset)
+    decodings = [processor.decode(ins) for ins in inputs["input_ids"][0]]   ##### remove this
+    torch.set_printoptions(profile="default")
     special_tokens_mask = inputs.pop("special_tokens_mask")
     special_tokens_mask = special_tokens_mask[0]
     answers = examples["answers"]
@@ -50,11 +57,15 @@ def preprocessFunction(examples):
     else:
         idx = context_start
         while idx<context_end and offset[idx][0] <=start_char:
+            tempOffset = offset[idx]
+            tempDecode = processor.decode(inputs["input_ids"][0][idx])
             idx += 1
         start_positions = idx-1
 
         idx = context_end
         while idx >= context_start and offset[idx][1] >= end_char:
+            tempOffset = offset[idx][0]
+            tempDecode = processor.decode(inputs["input_ids"][0][idx])
             idx -= 1
         end_positions = idx + 1
 
@@ -69,11 +80,12 @@ def preprocessFunction(examples):
 
     return inputs
 
-tokenized_dataset = dataset.map(preprocessFunction, remove_columns=dataset["train"].column_names)
+tokenized_dataset = dataset.map(preprocessFunction)  ################## add remove columns
 
 data_collator = DefaultDataCollator()
 model = MarkupLMForQuestionAnswering.from_pretrained("microsoft/markuplm-base-finetuned-websrc")
 
+# Training model
 training_args = TrainingArguments(
     output_dir = "my_qa_model",
     evaluation_strategy = "epoch",
@@ -84,7 +96,6 @@ training_args = TrainingArguments(
     weight_decay = 0.01,
     save_strategy="epoch",
 )
-
 trainer = Trainer(
     model=model,
     args=training_args,
@@ -93,7 +104,6 @@ trainer = Trainer(
     tokenizer=tokenizer,
     # data_collator = data_collator
 )
-
 trainer.train()
 
 tokenizer.save_pretrained("my_qa_model")
